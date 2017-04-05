31a5f2f922946192625b3895ed6891c8e86bbb3c
Benson Lee <blee@e1z2r5p11.42.us.org>
2017-04-05 13:42:33 -0700

    formatting

--- recursion_test.c
+++ recursion_test.c
@@ -1,34 +1,34 @@
-int recursion(char **board, char **valid, int row, int col)
+int		recursion(char **board, char **valid, int row, int col)
 {
-  if (!*valid)
-    return (0);
-  while (board[row])
-  {
-    col = 0;
-    while (board[row][col])
-    {
-      if (check_board(board, *valid, row, col))
-        {
-          put_tet(board, *valid, row, col);
-          if (recursion (board, (valid + 1), 0, 0))
-            remove_tet(board, *valid, 0 , 0);
-          else
-            return (0);
-        }
-        col++;
-    }
-    row++;
-  }
-  return (0);
+	if (!*valid)
+		return (0);
+	while (board[row])
+	{
+		col = 0;
+		while (board[row][col])
+		{
+			if (check_board(board, *valid, row, col))
+			{
+				put_tet(board, *valid, row, col);
+				if (recursion (board, (valid + 1), 0, 0))
+					remove_tet(board, *valid, 0 , 0);
+				else
+					return (0);
+			}
+			col++;
+		}
+		row++;
+	}
+	return (0);
 }
 
-char **loop_recursion(char **board, char **valid, int size)
+char	**loop_recursion(char **board, char **valid, int size)
 {
-  while (recursion(char board, valid, 0, 0))
-  {
-    size++;
-    free_board(board);
-    board = create_board(size);
-  }
-  return (board);
+	while (recursion(char board, valid, 0, 0))
+	{
+		size++;
+		free_board(board);
+		board = create_board(size);
+	}
+	return (board);
 }
