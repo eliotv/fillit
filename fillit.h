a3f4575dfdc7455bf3616ea1cb2759a8b8467123
Eliot vanHeumen <vanman1@gmail.com>
2017-04-05 16:52:39 -0700

    small changes to Makefile, header, added char func to valid

--- fillit.h
+++ fillit.h
@@ -1,4 +1,29 @@
 #ifndef FILLIT_H
 # define FILLIT_H
-#include "libft.h"
+#include "libft/libft.h"
 
+typedef	enum	e_bool;
+{
+		false, 
+		true
+}				t_bool;
+
+char	**create_board(int size);
+void	free_board(char **board);
+void	valid_tets(char **str);
+char	**set_valid();
+int	check_tet(char *str);
+int	check_board(charr **board, char *str, int row, int col);
+void	put_tet(char **board, char *str, int row, int col);
+void	remove_tet(char **board, char c, int row, int col);
+int	checkline(char *str);
+int	check_tet(char *str);
+int	check_tet_format(char *str);
+int	ft_strcmp_tet(const char *str1, const char *str2);
+void	valid_tets(char **str);
+int cmp_tet_input(char *src);
+int	readfile(char *file, int *tet_count);
+char	**ft_scan(int tet_count, char *str, char **valid);
+t_bool	recursion(char **board, char **tet_array, int row, int col);
+
+#endif
--- valid_tet.c
+++ valid_tet.c
@@ -1,30 +1,31 @@
-#include "libft/libft.h"
+#include "fillit.h"
 
-int		ft_strcmp_tet(const char *str1, const char *str2)
+int     ft_strcmp_tet(const char *str1, const char *str2)
 {
-	while (*str1 && *str2 && *str1 == *str2)
-	{
-		str1++;
-		str2++;
-		if (*str1 == '\n')
-			str1++;
-		if (*str2 == '\0')
-			return (1);
-	}
-	return (0);
+    while (*str1 && *str2 && *str1 == *str2)
+    {
+        str1++;
+        str2++;
+        if (*str1 == '\n')
+            str1++;
+        if (*str2 == '\0')
+            return (1);
+    }
+    return (0);
+
 }
 
-int		check_valid(char *src, char **valid)
+int check_valid(char *src, char **valid)
 {
-	int     i;
+    int     i;
 
-	i = -1;
-	while (++i < 19)
-	{
-		if (ft_strcmp_tet(src, valid[i]))
-			return (i);
-	}
-	return (-1);
+    i = -1;
+    while (++i < 19)
+    {
+        if (ft_strcmp_tet(src, valid[i]))
+            return (i);
+    }
+    return (-1);
 }
 
 char	**ft_scan(int tet_count, char *str, char **valid)
@@ -35,7 +36,7 @@ char	**ft_scan(int tet_count, char *str, char **valid)
 
 	i = 0;
 	row = 0;
-	if(!(tet_array = (char**)malloc(sizeof(char*) * tet_count)))
+	if(!(tet_array = (char**)malloc(sizeof(char*) * tet_count + 1)))
 		return (NULL);
 	while (tet_count--)
 	{
@@ -44,7 +45,7 @@ char	**ft_scan(int tet_count, char *str, char **valid)
 		{
 			if (!(tet_array[row] = ft_strnew(15)))
 				return (NULL);
-			ft_strcpy(tet_array[row], valid[i]);
+			ft_strcpy(tet_array[row], set_letter(valid[i], row));
 			row++;
 		}
 		else
@@ -53,4 +54,20 @@ char	**ft_scan(int tet_count, char *str, char **valid)
 	}
 	tet_array[row] = NULL;
 	return (tet_array);
+}
+
+char *set_letter(char *str, int j)
+{
+	int i;
+	char c;
+
+	c = 'A' + j;
+	i = 0;
+	while (str[i])
+	{
+		if(str[i] == '#')
+			str[i] = c;
+		i++;
+	}
+	return (str);
 }
